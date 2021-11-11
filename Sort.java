0a648c38f44f85a75c97e25f4db5fcbf2bdfcd5f
tama <tama.jobyuji@gmail.com>
2021-11-12 01:21:22 +0900

    Sortアルゴリズムの雛形作成

---
+++ Sort.java
@@ -0,0 +1,23 @@
+interface Sort{
+
+    void bubbleSort();
+    void bucketSort();
+    void heapSort();
+    void quickSort();
+    void selectSort();
+    void mergeSort();
+    void insertionSort();
+    void shellSort();
+    void shakerSort();
+    void comSort();
+    void gnomeSort();
+    void selfBalancingBinarySearchTreeSort();
+    void librarySort();
+    void inPlaceMergeSort();
+    void smoothSort();
+    void introSort();
+    void patienceSort();
+    void strandSort();
+    void oddEvenSort();
+    void shareSort();
+}
