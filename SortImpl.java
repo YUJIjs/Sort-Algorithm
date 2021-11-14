3a2dbef1f0f09225ae0d43013fc95b246515f364
tama <tama.jobyuji@gmail.com>
2021-11-14 13:17:52 +0900

    ListとMapをコンストラクタで受け取り、Implクラスで使用できるように実装

--- SortImpl.java
+++ SortImpl.java
@@ -1,127 +1,94 @@
-public class SortImpl implements Sort{
-    
-    SortImpl(){
+import java.util.List;
+import java.util.Map;
+
+public class SortImpl implements Sort {
+
+    SortImpl(List<Integer> createList, Map<Integer, String> refrectionList){
+       
+        List<Integer>List = createList;
+        Map<Integer,String> selectSort = refrectionList;
 
     }
-
+    
     @Override
     public void bubbleSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void bucketSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void heapSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void quickSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void selectSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void mergeSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void insertionSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void shellSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void shakerSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void comSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void gnomeSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void selfBalancingBinarySearchTreeSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void librarySort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void inPlaceMergeSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void smoothSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
-    public void introSort() {
-        // TODO Auto-generated method stub
-        
+    public void voidroSort() {
     }
 
     @Override
     public void patienceSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void strandSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void oddEvenSort() {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void shareSort() {
-        // TODO Auto-generated method stub
-        
     }
 
+    
 }
