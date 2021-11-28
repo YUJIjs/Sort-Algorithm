72b636fb6d1448499dd913182ec782b61e307a40
tama <tama.jobyuji@gmail.com>
2021-11-28 15:42:56 +0900

    Sort処理に関係ない部分を共通化

--- SortImpl.java
+++ SortImpl.java
@@ -25,97 +25,78 @@ public class SortImpl implements Sort {
 
     @Override
     public void heapSort() {
-        System.out.println(selectSort+"を開始します");
     }
 
     @Override
     public void quickSort() {
-        System.out.println(selectSort+"を開始します");
     }
 
     @Override
     public void selectSort() {
-        System.out.println(selectSort+"を開始します");
     }
 
     @Override
     public void mergeSort() {
-        System.out.println(selectSort+"を開始します");
     }
 
     @Override
     public void insertionSort() {
-        System.out.println(selectSort+"を開始します");
     }
 
     @Override
     public void shellSort() {
-        System.out.println("shellSort");
     }
 
     @Override
     public void shakerSort() {
-        System.out.println("shakerSort");
     }
 
     @Override
     public void comSort() {
-        System.out.println("comSort");
     }
 
     @Override
     public void gnomeSort() {
-        System.out.println("gnomeSort");
     }
 
     @Override
     public void selfBalancingBinarySearchTreeSort() {
-        System.out.println("selfBalancingBinarySearchTreeSort");
     }
 
     @Override
     public void librarySort() {
-        System.out.println("librarySort");
     }
 
     @Override
     public void inPlaceMergeSort() {
-        System.out.println("inPlaceMergeSort");
     }
 
     @Override
     public void smoothSort() {
-        System.out.println("smoothSort");
     }
 
     @Override
     public void voidroSort() {
-        System.out.println("voidroSort");
     }
 
     @Override
     public void patienceSort() {
-        System.out.println("bucketSort");
     }
 
     @Override
     public void strandSort() {
-        System.out.println("bucketSort");
     }
 
     @Override
     public void oddEvenSort() {
-        System.out.println("bucketSort");
     }
 
     @Override
     public void shareSort() {
-        System.out.println("bucketSort");
     }
 
     @Override
     public void introSort() {
-        System.out.println("bucketSort");
     }
 
     private static void printIterable(Iterator<Integer> iterator){
